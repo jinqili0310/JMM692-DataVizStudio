6de5667804924c7ff921737af36d1dd2c333619c
Jinqi Li <jxl2047@miami.edu>
2019-11-14 11:19:57 -0500

    first3pages

--- Ver1/main.js
+++ Ver1/main.js
@@ -1,7 +1,7 @@
 // Initiate scroll page
 new fullpage("#fullpage", {
   lockAnchors: false,
-  anchors: ["firstPage", "secondPage"],
+  anchors: ["1", "2", "3", "4", "5", "6", "7", "8"],
   navigation: true,
   navigationPosition: "right",
   navigationTooltips: [
@@ -18,9 +18,9 @@ new fullpage("#fullpage", {
   slidesNavigation: true,
   slidesNavPosition: "bottom",
   sectionsColor: [
-    "#fea3aa",
-    "#f8b88b",
-    "#faf884",
+    "#000000",
+    "#640303",
+    "#000000",
     "#baed91",
     "#f2a2e8",
     "#b2cefe",
@@ -70,3 +70,69 @@ function isElementInViewport(el) {
         (window.innerHeight || document.documentElement.clientHeight))
   );
 }
+
+const ImageLoop = (() => {
+  let s;
+
+  return {
+    settings() {
+      return {
+        image: document.querySelectorAll('.team__img'),
+        link: document.querySelectorAll('.team__link'),
+        intervalTime: 250
+      };
+    },
+
+    init() {
+      s = this.settings();
+      this.bindEvents();
+    },
+
+    bindEvents() {
+      this.hideImg();
+      this.hoverImg();
+    },
+
+    hideImg() {
+      [].forEach.call(s.image, img => {
+        [].forEach.call(img.children, (moreImg, idx) => {
+          if (idx !== 0) {
+            moreImg.style.display = 'none';
+          }
+        });
+      });
+    },
+
+    hoverImg() {
+      [].forEach.call(s.link, link => {
+        let interval;
+        let count = 0;
+
+        link.addEventListener('mouseenter', e => {
+          const target = e.target.children[0];
+          // Idx 1 because of the span tag/preloader
+          const img = target.children[1].children;
+          const length = img.length;
+
+          interval = setInterval(() => {
+            img[count].style.display = 'none';
+
+            if (count === length - 1) {
+              count = 0;
+            } else {
+              count++;
+            }
+
+            img[count].style.display = 'block';
+          }, s.intervalTime);
+        });
+
+        link.addEventListener('mouseleave', () => {
+          clearInterval(interval);
+        });
+      });
+    }
+  };
+})();
+
+ImageLoop.init();
